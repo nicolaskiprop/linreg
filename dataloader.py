f581f46bcf5084cfaab723252a03b9ee08b4f561
nicolaskiprop <yegonicolas@gmail.com>
2023-07-10 18:51:21 +0200

    prediction model done:fire:

--- dataloader.py
+++ dataloader.py
@@ -94,19 +94,19 @@ val_loss = history.history['val_loss']
 
 epochs_range=range(epochs)
 
-# plt.figure(figsize=(8,8))
-# plt.subplot(1,2,1)
-# plt.plot(epochs_range,acc,label='Training Accuracy')
-# plt.plot(epochs_range,val_acc,label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range,acc,label='Training Accuracy')
+plt.plot(epochs_range,val_acc,label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-# plt.subplot(1,2,2)
-# plt.plot(epochs_range,loss,label='Training Loss')
-# plt.plot(epochs_range,val_loss,label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
+plt.subplot(1,2,2)
+plt.plot(epochs_range,loss,label='Training Loss')
+plt.plot(epochs_range,val_loss,label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+#plt.show()
 
 
 #implementing data augmentation to figt overfitting
@@ -118,10 +118,72 @@ data_augmentation = keras.Sequential([
 ])
 
 #Visualize a few augmented examples by applying data augmentation to the same image several times:
-plt.figure(figsize=(10,10))
-for images,_ in train_ds.take(1):
-    for i in range(9):
-        augmented_images = data_augmentation(images)
-        ax = plt.subplot(3,3,i+1)
-        plt.imshow(augmented_images[0].numpy().astype('uint8'))
-        plt.axis('off')
+plt.figure(figsize=(10, 10))
+for images, _ in train_ds.take(1):
+  for i in range(9):
+    augmented_images = data_augmentation(images)
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(augmented_images[0].numpy().astype("uint8"))
+    plt.axis("off")
+#plt.show()
+
+#dropout
+model = Sequential([
+   data_augmentation,
+    layers.Rescaling(1./255),
+    layers.Conv2D(16,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(num_classes, name='outputs')
+])
+#compile and train the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.summary()
+epochs=15
+history = model.fit( train_ds,validation_data=val_ds,epochs=epochs)
+
+#visualize training results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range=range(epochs)
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range,acc,label='Training Accuracy')
+plt.plot(epochs_range,val_acc,label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1,2,2)
+plt.plot(epochs_range,loss,label='Training Loss')
+plt.plot(epochs_range,val_loss,label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+#predict on new data
+sunflower_url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg'
+sunflower_path = tf.keras.utils.get_file('Red_sunflower',origin=sunflower_url)
+
+img = tf.keras.utils.load_img(sunflower_path,target_size=(img_height,img_width))
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)#create a batch
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+   "This image most likely belongs to {} with a {:.2f} percent confidence." 
+   .format(class_names[np.argmax(score)],100*np.max(score))
+)
