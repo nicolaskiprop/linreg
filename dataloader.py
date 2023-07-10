2e79aaf4a2e63d7732e9fab5a2d857f8c684d176
nicolaskiprop <yegonicolas@gmail.com>
2023-07-10 15:18:43 +0200

    data augmentation

---
+++ dataloader.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+import pathlib
+import matplotlib.pyplot as plt
+
+
+dataset_url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz'
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+#create a dataset
+batch_size = 32
+img_height = 180
+img_width = 180
+
+#It's good practice to use a validation split when developing your model. Use 80% of the images for training and 20% for validation.
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset='training',
+    seed=123,
+    image_size=(img_height,img_width),
+    batch_size=batch_size
+)
+print(train_ds)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset='validation',
+    seed=123,
+    image_size=(img_height,img_width),
+    batch_size=batch_size
+)
+print(val_ds)
+
+class_names = train_ds.class_names
+print(class_names)
+
+for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
+#configure the dataset for performance using Dataset.cache() and Dataset.prefetch() methods.
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+#standardize the data
+import numpy as np
+from tensorflow import keras
+from keras import layers
+normalization_layer =layers.Rescaling(1./255)
+normalized_ds = train_ds.map(lambda x,y:(normalization_layer(x),y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+print(np.min(first_image),np.max(first_image))
+
+
+#Create the model
+from keras.models import Sequential
+num_classes = len(class_names)
+model = Sequential([
+    layers.Rescaling(1./255,input_shape=(img_height,img_width,3)),
+    layers.Conv2D(16,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,3,padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(num_classes)
+])
+
+#compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+#model.summary()
+
+#train the model with 10 epochs
+epochs=10
+history = model.fit( train_ds,validation_data=val_ds,epochs=epochs)
+
+#visualize the training results
+#creates plots for loss and accuracy on the training and validation sets.
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range=range(epochs)
+
+# plt.figure(figsize=(8,8))
+# plt.subplot(1,2,1)
+# plt.plot(epochs_range,acc,label='Training Accuracy')
+# plt.plot(epochs_range,val_acc,label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
+
+# plt.subplot(1,2,2)
+# plt.plot(epochs_range,loss,label='Training Loss')
+# plt.plot(epochs_range,val_loss,label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
+
+
+#implementing data augmentation to figt overfitting
+#we will use keras processing layers
+data_augmentation = keras.Sequential([
+    layers.RandomFlip('horizontal',input_shape=(img_height,img_width,3)),#horizontal flip
+    layers.RandomRotation(0.1),#randomly rotate the image
+    layers.RandomZoom(0.1),#randomly zoom the image
+])
+
+#Visualize a few augmented examples by applying data augmentation to the same image several times:
+plt.figure(figsize=(10,10))
+for images,_ in train_ds.take(1):
+    for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3,3,i+1)
+        plt.imshow(augmented_images[0].numpy().astype('uint8'))
+        plt.axis('off')
